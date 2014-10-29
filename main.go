33cf173b4cee22b153c251384afe9bc24a724358
walu <imcnan@gmail.com>
2014-10-29 15:08:40 +0800

    upgrade .

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
-package demo
+package mwiki
 
 import (
-	"github.com/erzha/http/server"
+	"github.com/erzha/kernel"
+	"code.google.com/p/go.net/context"
 )
 
-func abc() {
-	server.Boot()
+func appServerInit(ctx context.Context, pServer *kernel.Server) error {
+	return nil
 }
+
+func newAppHook() (interface{}, error) {
+	return nil, nil
+}
+
+func init() {
+	info := kernel.PluginInfo{}
+	info.Creater = newAppHook
+	info.ServerInit = appServerInit
+	kernel.RegisterPlugin("_app", info)
+}
+
---
+++ wiki/main.go
@@ -0,0 +1,9 @@
+package demo
+
+import (
+	"github.com/erzha/http/server"
+)
+
+func main() {
+	server.Boot()
+}
